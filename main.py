82e24d68840d76126eeb994d0a850bcbc674425f
Hollywood138rus <bagirovkamil757@gmail.com>
2024-02-11 17:21:42 +0300

    feat:new function

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-136//7
-print('Hello world')
+user_data = float(input('введите число: '))
+if user_data > 5.2 or user_data < 5.9:
+    print('number is bigger than 5.2 and number is smaller 5.9')
+else:
+    print('number is smaller than 5.2')
